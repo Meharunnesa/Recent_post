6b0d6590778b3b93ec52a32afea8c84af71d869b
Meharunnesa <meharunnesab28@gmail.com>
2025-03-29 19:36:25 +0600

    Recently published post

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * Plugin Name:       Recently published posts
+ * Description:       This plugin helps users quickly monitor their latest content
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Bristy
+ * Text Domain:       RPP
+*/
+
+
+
+function display_post_widget() {
+
+
+    wp_add_dashboard_widget(
+        'display_post_widget',            // Widget slug
+        'Recently Published Post',          // Title
+        'display_post'                    // Display function
+    );
+}
+add_action('wp_dashboard_setup', 'display_post_widget');
+  
+  
+  
+  
+function display_post() {
+
+   $posts = get_posts( array(
+        'post_type' => 'post',
+        'post_status' => 'publish',
+        'orderby'        => 'asc',
+        'numberposts' => 3,
+      ) );
+      
+    // print_r($posts);
+
+    if (!empty($posts)) {
+        echo '<ul>';
+        foreach ($posts as $post) {
+            echo '<li>';
+            echo '<strong><a href="' . get_permalink($post->ID) . '" target="_blank">' . esc_html($post->post_title) . '</a></strong>';
+            echo ' - <small>' . get_the_date('F j, Y g:i A', $post->ID) . '</small>';
+            echo '</li>';  
+        }
+        echo '</ul>';
+    } else {
+        echo 'No recent posts found.';
+    }
+  
+}
+  
+
+
+?>
